72e641c27afcad7b10f288e94e2b688b8d86629b
mpgxc <mpgx5.c@gmail.com>
2020-06-05 20:17:09 -0300

    feat: add *.css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import './App.css';
+import Routes from './routes';
 
 export default function App() {
-  return (
-
-    <h1>Olá, Mundo!</h1>
-
-  );
+  return <Routes />
 }
---
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles.css';
+
+const CreatePoint: React.FC = () => {
+  return <div />;
+}
+
+export default CreatePoint;
