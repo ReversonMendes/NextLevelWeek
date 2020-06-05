04b234fd660f25b0e1c2f21045f11c0965886477
mpgxc <mpgx5.c@gmail.com>
2020-06-05 01:27:47 -0300

    fix: clean the project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,8 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Olá, Mundo!</h1>
-      </header>
-    </div>
+
+    <h1>Olá, Mundo!</h1>
+
   );
 }
